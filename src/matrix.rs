3f8a264bc60cd15311d54fa7fcb11748a1ce5678
sklbz <sokol.bozanic@tumo.world>
2025-03-29 22:28:00 +0100

    added more methods to custom types to allow for various NN architecture

--- src/matrix.rs
+++ src/matrix.rs
@@ -5,11 +5,25 @@ pub type Matrix<T> = Vector<Vector<T>>;
 pub type Tensor<T> = Vector<Matrix<T>>;
 
 trait Random {
-    fn random(size: (usize, usize, usize)) -> Self;
+    fn random_static_size(size: (usize, usize, usize)) -> Self;
+    fn random_dynamic_size(size: (usize, Vec<usize>, Vec<usize>)) -> Self;
+   
 }
 
 impl Random for Vector<f64> {
-    fn random(size: (usize, usize, usize)) -> Self {
+    fn random_static_size(size: (usize, usize, usize)) -> Self {
+        let mut rng = rand::rng();
+
+        let mut vec = Vec::new();
+        for _ in 0..size.0 {
+            vec.push(rng.random::<f64>());
+        }
+
+        vec
+    }
+
+    // This is actually the exact same but I don't know how to collapse them
+    fn random_dynamic_size(size: (usize, Vec<usize>, Vec<usize>)) -> Self {
         let mut rng = rand::rng();
 
         let mut vec = Vec::new();
@@ -22,11 +36,22 @@ impl Random for Vector<f64> {
 }
 
 impl Random for Matrix<f64> {
-    fn random(size: (usize, usize, usize)) -> Self {
+    fn random_static_size(size: (usize, usize, usize)) -> Self {
         let mut matrix = Vector::new();
 
         for _ in 0..size.0 {
-            let row = Vector::random((size.1, 1, 1))
+            let row = Vector::random_static_size((size.1, 1, 1))
+            matrix.push(row);
+        }
+
+        matrix
+    }
+
+    fn random_dynamic_size(size: (usize, Vec<usize>, Vec<usize>)) -> Self {
+        let mut matrix = Vector::new();
+
+        for i in 0..size.0 {
+            let row = Vector::random_static_size((size.1[i], 1, 1));
             matrix.push(row);
         }
 
@@ -35,14 +60,22 @@ impl Random for Matrix<f64> {
 }
 
 impl Random for Tensor<f64> {
-    fn random(size: (usize, usize, usize)) -> Self {
+    fn random_static_size(size: (usize, usize, usize)) -> Self {
         let mut tensor = Vector::new();
 
         for _ in 0..size.0 {
-            let matrix = Matrix::random((size.1, size.2, 1));
+            let matrix = Matrix::random_static_size((size.1, size.2, 1));
             tensor.push(matrix);
         }
 
         tensor
     }
+
+    fn random_dynamic_size(size: (usize, Vec<usize>, Vec<usize>)) -> Self {
+        let mut tensor = Vector::new();
+
+        for i in 0..size.0 {
+            let matrix = Matrix::random_static_size((size.1[i], size.2[i], 1));
+            tensor.push(matrix);
+        }
 }
