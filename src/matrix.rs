fa31210185c1a7bdd4c9c89ae79bcdb74e974349
sklbz <sokol.bozanic@tumo.world>
2025-03-30 12:39:25 +0200

    added matrix transpose

--- src/matrix.rs
+++ src/matrix.rs
@@ -8,6 +8,8 @@ pub type Tensor<T> = Vector<Matrix<T>>;
 pub type PseudoMatrix<T> = Vector<Vector<T>>;
 pub type PseudoTensor<T> = Vector<Matrix<T>>;
 
+//------------------------------------------------------------------------------
+
 pub trait Length {
     fn size(&self) -> impl Size;
 }
@@ -29,6 +31,30 @@ impl Length for Tensor<f64> {
         (self.len(), self[0].len(), self[0][0].len())
     }
 }
+
+//------------------------------------------------------------------------------
+
+pub trait Transpose {
+    fn transpose(&self) -> Self;
+}
+
+impl Transpose for Matrix<f64> {
+    fn transpose(&self) -> Self {
+        let mut matrix = Vector::new();
+
+        for i in 0..self[0].len() {
+            let mut row = Vector::new();
+            for j in 0..self.len() {
+                row.push(self[j][i]);
+            }
+            matrix.push(row);
+        }
+
+        matrix
+    }
+}
+
+//------------------------------------------------------------------------------
 
 pub trait Random {
     fn random<T: Size>(size: T) -> Self;
