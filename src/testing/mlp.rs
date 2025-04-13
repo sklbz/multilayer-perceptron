8084b783cca3ed25346b973c91e4c70da9810784
sklbz <sokol.bozanic@tumo.world>
2025-04-13 16:30:06 +0200

    NN can do calculation

--- src/testing/mlp.rs
+++ src/testing/mlp.rs
@@ -2,5 +2,7 @@ use crate::multilayer_perceptron::multilayer_perceptron::MultiLayerPerceptron;
 
 pub fn test() {
     let mlp = MultiLayerPerceptron::new(vec![2, 3, 1]);
-    mlp.display();
+    let calc = mlp.calc(vec![0.0, 0.0]);
+
+    println!("\nCalc: {:?}", calc);
 }
