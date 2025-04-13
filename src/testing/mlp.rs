9ac740f726874d6f2603e899df2f4b829ff5a25f
sklbz <sokol.bozanic@tumo.world>
2025-04-13 17:29:53 +0200

    started a very crude implementation of gradient descent for testing purposes

--- src/testing/mlp.rs
+++ src/testing/mlp.rs
@@ -1,8 +1,13 @@
 use crate::multilayer_perceptron::multilayer_perceptron::MultiLayerPerceptron;
 
 pub fn test() {
-    let mlp = MultiLayerPerceptron::new(vec![2, 3, 1]);
+    let mut mlp = MultiLayerPerceptron::new(vec![2, 64, 64, 64, 64, 3]);
+
     let calc = mlp.calc(vec![0.0, 0.0]);
 
-    println!("\nCalc: {:?}", calc);
+    println!("Calculation: {:?}", calc);
+
+    let database = vec![(vec![0.0, 0.0], vec![1.0, 0.0, 0.0], 1.0)];
+
+    mlp.train(database);
 }
