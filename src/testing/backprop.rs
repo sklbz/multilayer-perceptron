f97a19f093001ab5f4a3782b08622bcac9fe0677
sklbz <sokol.bozanic@tumo.world>
2025-04-20 19:45:47 +0200

    added parameters for iterations and momentum

--- src/testing/backprop.rs
+++ src/testing/backprop.rs
@@ -7,19 +7,19 @@ use std::time::Duration;
 
 #[allow(unused)]
 pub fn test() {
-    let mut mlp = MultiLayerPerceptron::new(vec![2, 3, 4, 1]);
+    let mut mlp = MultiLayerPerceptron::new(vec![2, 1]);
 
     let database = vec![(vec![0f64, 0f64], vec![0f64], 1f64)];
 
     let test_input = vec![0f64, 0f64];
 
     for i in 1..=50 {
-        mlp.backpropagation(database.clone(), 10_000 * i, 0.1 / i as f64);
+        mlp.backpropagation(database.clone(), 100, 0.1);
 
         let test = mlp.calc(test_input.clone());
 
         println!();
-        println!("TEST : {:?}", test);
+        println!("TEST {i}: {:?}", test);
         // sleep(Duration::from_millis(50));
     }
 }
