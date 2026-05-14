9272fda338bfcbc0e5af2a19f82ddf2ec29310c0
sklbz <sokol.bozanic@tumo.world>
2026-05-14 09:18:48 +0200

    update

--- src/mlp/activation/rectified_linear.rs
+++ src/mlp/activation/rectified_linear.rs
@@ -1,1 +1,9 @@
-pub struct ReLU{}
+pub fn relu(x: f64) -> f64 {
+    if x > 0.0 { x } else { 0.0 }
+}
+
+pub fn RELU(v: Vec<f64>) -> Vec<f64> {
+    v.iter()
+        .map(|coord: &f64| relu(*coord))
+        .collect::<Vec<f64>>()
+}
